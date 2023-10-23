1bc75f97ba7b681092b1682828d5394305beb347
Дмитрий Анатольевич Костюченко <141655594+DarthGreenman@users.noreply.github.com>
2023-10-23 17:44:19 +0300

    Added project files

---
+++ simulator/includes/racer.h
@@ -0,0 +1,16 @@
+﻿/* racer.h */
+
+#ifndef RACE_SIMULATOR_RACERS_H
+#define RACE_SIMULATOR_RACERS_H
+
+#include "vehicle.h"
+
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace race
+{
+    std::unique_ptr<vehicles::Vehicle> make_racer(char bar_key);
+}
+#endif /* RACE_SIMULATOR_RACERS_H */
